9f1af6f1c05611ec73238b22bb6af04d727fd427
tokinarazu <tokinarazu@gmail.com>
2024-10-05 21:50:47 +0900

    fix compile error

--- qmk_firmware/keyboards/keyball/drivers/pmw3360/pmw3360.h
+++ qmk_firmware/keyboards/keyball/drivers/pmw3360/pmw3360.h
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdint.h>
 #include "spi_master.h"
+#include "gpio.h"
 
 //////////////////////////////////////////////////////////////////////////////
 // Configurations
