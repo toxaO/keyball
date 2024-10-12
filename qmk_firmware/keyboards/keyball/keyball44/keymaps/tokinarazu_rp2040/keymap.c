20ab79d72acb38f8728ad791a1e2f7b820278955
tokinarazu <tokinarazu@gmail.com>
2024-10-12 20:06:30 +0900

    TAP_DANCE_ENABLE 追加

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tokinarazu_rp2040/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tokinarazu_rp2040/keymap.c
@@ -324,7 +324,7 @@ void set_disable_ime(void) {
 }
 
 // [q]二回押しで[Esc]
-void dance_q_finished(qk_tap_dance_state_t *state, void *user_data) {
+void dance_q_finished(tap_dance_state_t *state, void *user_data) {
   if (state->count == 1) {
     register_code16(KC_Q);
   } else {
@@ -332,7 +332,7 @@ void dance_q_finished(qk_tap_dance_state_t *state, void *user_data) {
   }
 }
 
-void dance_q_reset(qk_tap_dance_state_t *state, void *user_data) {
+void dance_q_reset(tap_dance_state_t *state, void *user_data) {
   if (state->count == 1) {
     unregister_code16(KC_Q);
   } else {
@@ -340,7 +340,7 @@ void dance_q_reset(qk_tap_dance_state_t *state, void *user_data) {
   }
 }
 
-qk_tap_dance_action_t tap_dance_actions[] = {
+tap_dance_action_t tap_dance_actions[] = {
   [TD_Q_ESC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_q_finished, dance_q_reset),
 };
 
